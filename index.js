f5a1e9464da2a59e4220585fef3f75fd46fdb3c2
Shakil <shakil11271@gmail.com>
2022-05-02 12:34:04 +0600

    loading all data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -14,7 +14,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.od1ou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect();
+        const inventoryItems = client.db('carDealer').collection('inventory');
 
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryItems.find(query);
+            const inventory = await cursor.toArray();
+            res.send(inventory);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 app.get('', (req, res) => {
