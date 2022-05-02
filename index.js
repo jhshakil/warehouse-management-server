6b776521f28e844801374a3817eca913eb2eb24e
Shakil <shakil11271@gmail.com>
2022-05-02 11:23:45 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,6 +8,14 @@ const app = express();
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.od1ou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 
 app.get('', (req, res) => {
     res.send('server is running');
